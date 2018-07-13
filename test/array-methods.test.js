c350cb911b813d37539816d5b735632e91654721
MariahAdams <mariah.adams23@yahoo.com>
2018-07-13 11:05:38 -0700

    commit for travis

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
-const { arrayMethod } = require('../lib/array-methods');
+// const { arrayMethod } = require('../lib/array-methods');
 
 describe('array methods', () => {
 
-    it('Don\'t ask why', () => {
+    let words = ['sky', 'hot', 'Yeti', 'green'];
 
+    it('takes an array of words and returns a new array that has removed words that contain Y/y', () => {
+        const expected = ['hot', 'green'];
+        assert.equal(words, expected);
     });
 });
